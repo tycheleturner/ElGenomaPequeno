b3cacd29fcbfc43f5afe0ce270ae330915caa904
Tychele Turner <tycheleturner@gmail.com>
2017-09-19 15:56:30 -0700

    [script] Updated modules

--- cluster_analysis.sh
+++ cluster_analysis.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+module purge
+module load modules modules-init modules-gs modules-eichler mpfr/3.1.0 mpc/0.8.2 gmp/5.0.2 gcc/4.9.1 anaconda/20161130 R/3.0.0
+
 cd mitobam
 snakemake --cluster 'qsub {params.sge_opts}' -j 100 -s mitobam.snake -w 60 --rerun-incomplete -k
 snakemake -s mitobam.snake clean
