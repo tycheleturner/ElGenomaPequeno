6da49d1a2a7e4c7596b91feaedef7a9a6656f2b3
Turner <tychele@compute1-client-2.ris.wustl.edu>
2020-06-28 14:35:11 -0500

    [scripts] updates scripts to be docker compatible

--- map/map_haplogroup.sh
+++ map/map_haplogroup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #author: Tychele N. Turner, Ph.D.
 #email: tycheleturner@gmail.com
-Rscript map_haplogroup.R -i ../haplogroup/haplogroups_for_mapper.txt -o haplogroup_map.pdf -c coordinates.txt
+/opt/conda/bin/Rscript map_haplogroup.R -i ../haplogroup/haplogroups_for_mapper.txt -o haplogroup_map.pdf -c coordinates.txt
